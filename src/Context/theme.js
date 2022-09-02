02c054b65d93416b17fdca5714ff5110fc9dd448
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-09-02 16:02:27 +0300

    create theme context

---
+++ src/Context/theme.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import { createContext } from 'react';
+
+export const ThemeContext = createContext({
+  theme: 'light',
+  setTheme: () => {},
+});
