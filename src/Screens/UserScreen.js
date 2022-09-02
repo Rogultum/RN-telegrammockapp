218d7db571e83080dbe2d7061d09f1dd8b41e2f4
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-09-03 00:48:12 +0300

    add user screen(UNUSED ATM)

---
+++ src/Screens/UserScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function UserScreen({ route }) {
+  return (
+    <View>
+      <Text>USER</Text>
+    </View>
+  );
+}
+
+export default UserScreen;
