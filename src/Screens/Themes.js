b52706ec29d740e49345f551e483cc6412f51cc5
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-09-03 00:48:53 +0300

    add themes for screen for changing theme(UNFINISHED)

---
+++ src/Screens/Themes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function Themes() {
+  return (
+    <View>
+      <Text>Press</Text>
+    </View>
+  );
+}
+
+export default Themes;
